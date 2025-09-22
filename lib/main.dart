ec5007026606282dcc7671ac23a8bb7109c9269f
Abel Abebe <abelabebe313@gmail.com>
2025-09-22 16:58:58 +0300

    Lock orientation to portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'app/app.dart';
 import 'app/service_locator.dart';
@@ -6,6 +7,13 @@ import 'app/service_locator.dart';
 void main() async {
   setupLocator();
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Lock orientation to portrait mode only
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  
   await initHiveForFlutter();
   runApp(const MyApp());
 }
